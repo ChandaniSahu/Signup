9b9000ec8a8f66f2d36e931d7b551227797d0afa
ChandaniSahu <rrrsahu2005@gmail.com>
2025-12-24 12:38:46 +0530

    Readme.md file deletion successfull

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -6,6 +6,10 @@ const Home = () => {
     <>
     <Link to='/signup'>Signup</Link>
     <Link to='/login'>Login</Link>
+
+  <h1>Hi! I am Chandani Sahu</h1>
+  <h3>This is basic signup and login page that<br/>
+    I build to learn basic signup and login functionality in any web application</h3>
     </>
   )
 }
